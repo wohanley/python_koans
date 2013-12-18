366db67b366cefd59212f397810d580ee3ac542e
William O'Hanley <willy.ohanley@gmail.com>
2013-12-18 12:44:22 -0400

    triangles, done without conditionals

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -16,9 +16,16 @@
 # and
 #   about_triangle_project_2.py
 #
+
+# key: number of unique side lengths
+triangleTypes = {
+    1: 'equilateral',
+    2: 'isosceles',
+    3: 'scalene'
+}
+
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    return triangleTypes[len({a, b, c})]
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
